24b9421afbc8afad5e5e082605a7f8fb15608a47
ycycwx <yytcjcy@gmail.com>
2021-11-08 23:10:44 +0800

    chore: remove useless async mark

--- src/graphql/api.ts
+++ src/graphql/api.ts
@@ -2,7 +2,7 @@ import {graphql} from 'graphql';
 import {rootValue} from './rootValue';
 import {schema} from './schema';
 
-export const request = async <T = any>(source: string, variableValues?: any) => graphql({
+export const request = <T = any>(source: string, variableValues?: any) => graphql({
     schema,
     source,
     rootValue,
