1b5a9cec963d93f2d8c8fb616b0a26be497ec975
ycycwx <yytcjcy@gmail.com>
2024-10-03 20:00:51 +0800

    feat: upgrade to vite 5.0 & migrate package with type: module

---
+++ server.js
@@ -0,0 +1,60 @@
+import fs from 'node:fs/promises';
+import express from 'express';
+
+const isProd = process.env.NODE_ENV === 'production';
+const port = process.env.PORT || 8080;
+const base = process.env.BASE || '/';
+
+const app = express();
+
+/**
+ * @type {import('vite').ViteDevServer}
+ */
+let vite;
+if (isProd) {
+    const compression = (await import('compression')).default;
+    app.use(compression());
+
+    const serveStatic = (await import('serve-static')).default;
+    app.use(serveStatic('./dist/client', {index: false}));
+}
+else {
+    const {createServer} = await import('vite');
+    vite = await createServer({
+        base,
+        logLevel: 'info',
+        appType: 'custom',
+        server: {
+            middlewareMode: true,
+            watch: {
+                usePolling: true,
+                interval: 100,
+            },
+        },
+    });
+    app.use(vite.middlewares);
+}
+
+app.use('*', async (req, res) => {
+    try {
+        const url = req.originalUrl.replace(base, '');
+        const manifest = isProd ? await fs.readFile('./dist/client/manifest.json', 'utf-8') : undefined;
+        const render = isProd
+            // eslint-disable-next-line import/extensions
+            ? (await import('./dist/server/server.mjs')).render
+            : (await vite.ssrLoadModule('/src/server.tsx')).render;
+
+        render(url, req, res, manifest);
+    }
+    catch (e) {
+        !isProd && vite.ssrFixStacktrace(e);
+        // eslint-disable-next-line no-console
+        console.log(e.stack);
+        res.status(500).end(e.stack);
+    }
+});
+
+app.listen(port, () => {
+    // eslint-disable-next-line no-console
+    console.log(`Server is running in port ${port}`);
+});
