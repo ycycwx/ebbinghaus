b96aef54eec7187983f5d49c63bf42a4242db45f
ycycwx <yytcjcy@gmail.com>
2025-03-02 00:21:20 +0800

    fix: ignore dist to be linted

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,15 @@
 import {flatConfigs} from '@yotsubacy/config/eslint';
 
-export default flatConfigs.config(['node', 'browser', 'react', 'typescript'], {
-    languageOptions: {
-        parserOptions: {
-            tsconfigRootDir: import.meta.dirname,
+export default flatConfigs.config(
+    ['node', 'browser', 'react', 'typescript'],
+    {
+        languageOptions: {
+            parserOptions: {
+                tsconfigRootDir: import.meta.dirname,
+            },
         },
     },
-});
+    {
+        ignores: ['dist'],
+    }
+);
