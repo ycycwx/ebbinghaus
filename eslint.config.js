8ea03f14f3bc6c9b1af023453ab2a351001858b7
ycycwx <yytcjcy@gmail.com>
2025-03-01 16:53:25 +0800

    chore: upgrade @yotsubacy/config

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,9 @@
 import {flatConfigs} from '@yotsubacy/config/eslint';
 
-export default flatConfigs.build(
-    [
-        'node',
-        'browser',
-        'react',
-        'typescript',
-    ],
-    {
-        tsconfigRootDir: import.meta.dirname,
-    }
-);
+export default flatConfigs.config(['node', 'browser', 'react', 'typescript'], {
+    languageOptions: {
+        parserOptions: {
+            tsconfigRootDir: import.meta.dirname,
+        },
+    },
+});
