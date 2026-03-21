591dbabb809ea3fe4778c600b8165bc693a8e935
ycycwx <yytcjcy@gmail.com>
2026-03-21 15:57:45 +0800

    chore: upgrade vite 8

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import {defineConfig} from 'vite';
-import react from '@vitejs/plugin-react';
+import react, {reactCompilerPreset} from '@vitejs/plugin-react';
+import babel from '@rolldown/plugin-babel';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
-        react({
-            babel: {
-                plugins: ['babel-plugin-react-compiler'],
-            },
+        react(),
+        babel({
+            plugins: [reactCompilerPreset],
         }),
     ],
     build: {
--- vite.vercel.config.ts
+++ vite.vercel.config.ts
@@ -1,11 +1,15 @@
 import {defineConfig} from 'vite';
-import react from '@vitejs/plugin-react';
+import react, {reactCompilerPreset} from '@vitejs/plugin-react';
+import babel from '@rolldown/plugin-babel';
 import {vercel} from './plugins/vercel';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         react(),
+        babel({
+            plugins: [reactCompilerPreset],
+        }),
         vercel(),
     ],
     build: {
